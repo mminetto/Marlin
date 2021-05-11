fb99af08d1a0830e4e3128f00dc6328b58555a0e
Heber Minetto <heber@minetto.dev.br>
2021-05-10 21:37:43 -0300

    ajustando passos por mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -811,7 +811,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 142 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 98.5, 402, 142 }
 
 /**
  * Default Max Feed Rate (mm/s)
